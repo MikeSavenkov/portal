efee2fd6181bc867b76987ff88e90700707d474c
Mike <savenkovmike@gmail.com>
2022-04-27 14:21:01 +0300

    refactoring

--- src/main/java/com/fides/portal/controller/MainController.java
+++ src/main/java/com/fides/portal/controller/MainController.java
@@ -1,14 +1,12 @@
 package com.fides.portal.controller;
 
 import com.fides.portal.domain.Employee;
-import com.fides.portal.repos.EmployeeRepo;
 import com.fides.portal.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
@@ -16,8 +14,6 @@ import java.util.Map;
 @Controller
 public class MainController {
 
-    @Autowired
-    private EmployeeRepo employeeRepo;
     private final EmployeeService employeeService;
     @Autowired
     public MainController(EmployeeService employeeService) {
@@ -25,8 +21,8 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
-        return "greeting";
+    public String main(Map<String, Object> model) {
+        return "main";
     }
     @GetMapping("/employees")
     public String findAll(Map<String, Object> model) {
@@ -64,38 +60,5 @@ public class MainController {
         employeeService.saveEmployee(employee);
         return "redirect:/employees";
     }
-
-    @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        Iterable<Employee> employees = employeeService.findAll();
-
-        model.put("employees", employees);
-        return "main";
-    }
-
-//    @PostMapping("/main")
-//    public String add(@RequestParam String firstName, @RequestParam String lastName, Long id, Map<String, Object> model) {
-//        Employee employee = new Employee(firstName, lastName, id);
-//        employeeRepo.save(employee);
-//
-//        Iterable<Employee> employees = employeeService.findAll();
-//        model.put("employees", employees);
-//
-//        return "main";
-//    }
-
-//    @PostMapping("filter")
-//    public String filter(@RequestParam String filter, Map<String, Object> model) {
-//        Iterable<Employee> employees;
-//
-//        if (filter != null && !filter.isEmpty()) {
-//            employees = employeeRepo.findBySurname(filter);
-//        } else {
-//            employees = employeeService.findAll();
-//        }
-//
-//        model.put("employees", employees);
-//        return "main";
-//    }
 
 }
